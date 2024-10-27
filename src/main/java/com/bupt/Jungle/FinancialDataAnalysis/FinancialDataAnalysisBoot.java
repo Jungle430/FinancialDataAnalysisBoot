5fe35aa681f691d45b095af3b14186314346dde7
Jungle430 <junglece430@gmail.com>
2024-10-27 21:43:55 +0800

    init

---
+++ src/main/java/com/bupt/Jungle/FinancialDataAnalysis/FinancialDataAnalysisBoot.java
@@ -0,0 +1,13 @@
+package com.bupt.Jungle.FinancialDataAnalysis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialDataAnalysisBoot {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinancialDataAnalysisBoot.class, args);
+    }
+
+}
---
+++ src/test/java/com/bupt/Jungle/FinancialDataAnalysis/FinancialDataAnalysisBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bupt.Jungle.FinancialDataAnalysis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialDataAnalysisBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
