2bc2f4c660ac018fa9d0fd4dedb183ed459fe072
Jungle430 <junglece430@gmail.com>
2024-11-21 23:11:56 +0800

    Result DTO

---
+++ src/main/java/com/bupt/Jungle/FinancialDataAnalysis/constant/ResultCodeConstant.java
@@ -0,0 +1,27 @@
+package com.bupt.Jungle.FinancialDataAnalysis.constant;
+
+public final class ResultCodeConstant {
+    /**
+     * {@code 200 OK} (HTTP/1.0 - RFC 1945)
+     */
+    public static final int OK = 200;
+
+    /**
+     * {@code 404 Not Found} (HTTP/1.0 - RFC 1945)
+     */
+    public static final int NOT_FOUND = 404;
+
+    public static final String NOT_FOUND_MESSAGE = "404 Not Found";
+
+    /**
+     * {@code 500 Server Error} (HTTP/1.0 - RFC 1945)
+     */
+    public static final int SERVER_ERROR = 500;
+
+    /**
+     * {@code 510 no auth}
+     */
+    public static final int NO_AUTH = 510;
+
+    public static final String NO_AUTH_MESSAGE = "No authentication, need login";
+}
---
+++ src/main/java/com/bupt/Jungle/FinancialDataAnalysis/util/Result.java
@@ -0,0 +1,73 @@
+package com.bupt.Jungle.FinancialDataAnalysis.util;
+
+import com.bupt.Jungle.FinancialDataAnalysis.constant.ResultCodeConstant;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@ToString
+@EqualsAndHashCode
+@Schema(name = "向前端返回包装类型")
+public class Result<T> {
+    @Schema(description = "是否成功")
+    private boolean success;
+
+    @Schema(description = "错误信息")
+    private String errMessage;
+
+    @Schema(description = "状态码")
+    private int code;
+
+    @Schema(description = "数据")
+    private T data;
+
+    @Schema(description = "时间戳")
+    private long timestamp;
+
+    public Result() {
+    }
+
+    public static <T> Result<T> ok(T data) {
+        Result<T> result = new Result<>();
+        result.setSuccess(true);
+        result.setErrMessage(null);
+        result.setCode(ResultCodeConstant.OK);
+        result.setData(data);
+        result.setTimestamp(System.currentTimeMillis());
+        return result;
+    }
+
+    public static <T> Result<T> fail(String errMessage) {
+        Result<T> result = new Result<>();
+        result.setSuccess(false);
+        result.setErrMessage(errMessage);
+        result.setCode(ResultCodeConstant.SERVER_ERROR);
+        result.setData(null);
+        result.setTimestamp(System.currentTimeMillis());
+        return result;
+    }
+
+    public static <T> Result<T> notFound() {
+        Result<T> result = new Result<>();
+        result.setSuccess(false);
+        result.setErrMessage(ResultCodeConstant.NOT_FOUND_MESSAGE);
+        result.setCode(ResultCodeConstant.NOT_FOUND);
+        result.setData(null);
+        result.setTimestamp(System.currentTimeMillis());
+        return result;
+    }
+
+    public static <T> Result<T> noAuth() {
+        Result<T> result = new Result<>();
+        result.setSuccess(false);
+        result.setErrMessage(ResultCodeConstant.NO_AUTH_MESSAGE);
+        result.setCode(ResultCodeConstant.NO_AUTH);
+        result.setData(null);
+        result.setTimestamp(System.currentTimeMillis());
+        return result;
+    }
+}
