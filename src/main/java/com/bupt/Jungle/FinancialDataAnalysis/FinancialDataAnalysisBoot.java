3bd29c8c089da2c550114d2573a0b2f754a3f698
Jungle430 <junglece430@gmail.com>
2024-12-06 01:23:51 +0800

    feat(*): turn on the aop proxy

--- src/main/java/com/bupt/Jungle/FinancialDataAnalysis/FinancialDataAnalysisBoot.java
+++ src/main/java/com/bupt/Jungle/FinancialDataAnalysis/FinancialDataAnalysisBoot.java
@@ -3,9 +3,11 @@ package com.bupt.Jungle.FinancialDataAnalysis;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
 @MapperScan("com.bupt.Jungle.FinancialDataAnalysis.infrastructure.dal.mapper")
+@EnableAspectJAutoProxy
 public class FinancialDataAnalysisBoot {
 
     public static void main(String[] args) {
