2cb1ce9a319902172dadba1bd7ad996f9b6b8bf4
Jungle430 <junglece430@gmail.com>
2024-12-07 14:48:49 +0800

    fix(*): fix the controller mapping url

--- src/main/java/com/bupt/Jungle/FinancialDataAnalysis/starter/controller/UserController.java
+++ src/main/java/com/bupt/Jungle/FinancialDataAnalysis/starter/controller/UserController.java
@@ -16,12 +16,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/user")
+@RestController
+@RequestMapping("/user")
 @Tag(name = "用户")
-public class  UserController {
+public class UserController {
     private final UserLogService userLogService;
 
     private final UserInfoService userInfoService;
