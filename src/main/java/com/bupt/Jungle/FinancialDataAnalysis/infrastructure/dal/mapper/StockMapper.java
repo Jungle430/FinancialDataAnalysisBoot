4c1678c2dd60df26a5e231bf7778f24b50ba1752
Jungle430 <junglece430@gmail.com>
2024-12-19 01:48:12 +0800

    feat(*): add the stock table query

--- src/main/java/com/bupt/Jungle/FinancialDataAnalysis/infrastructure/dal/mapper/StockMapper.java
+++ src/main/java/com/bupt/Jungle/FinancialDataAnalysis/infrastructure/dal/mapper/StockMapper.java
@@ -1,5 +1,6 @@
 package com.bupt.Jungle.FinancialDataAnalysis.infrastructure.dal.mapper;
 
+import com.bupt.Jungle.FinancialDataAnalysis.infrastructure.dal.model.StockPO;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
@@ -11,4 +12,24 @@ public interface StockMapper {
     List<String> queryAllCurrency();
 
     List<String> queryAllMarketRegion();
+
+    List<StockPO> queryStockTag(
+            String code,
+            String platform,
+            String region,
+            String currency,
+            String name,
+            String marketRegion,
+            Long limitSize,
+            Long offSet
+    );
+
+    Long queryStockTagTotalCount(
+            String code,
+            String platform,
+            String region,
+            String currency,
+            String name,
+            String marketRegion
+    );
 }
